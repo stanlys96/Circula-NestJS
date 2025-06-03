6d62201826ccf9fdc44b7d07fed119004fbcddc6
stanly-photon <stanly.sukmajaya@photon.com>
2025-06-03 13:58:40 -0500

    wlao

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -4,9 +4,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ChatMessage } from './entities/chat.entity';
 import { ChatService } from './chat.service';
 import { ChatGateway } from './chat.gateway';
+import { ChatController } from './chat.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([ChatMessage])],
   providers: [ChatService, ChatGateway],
+  controllers: [ChatController],
+  exports: [ChatService],
 })
 export class ChatModule {}
